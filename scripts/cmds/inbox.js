42d580cdffe78288bea346ed12959e5fbe2b37de
RAISA 6T9 <maxjihad59@gmail.com>
2025-01-09 18:32:52 +0600

    Create inbox.js

---
+++ scripts/cmds/inbox.js
@@ -0,0 +1,37 @@
+module.exports = {
+  config: {
+    name: "inbox",
+    aliases: ["in"],
+    version: "1.0",
+    author: "MOHAMMAD BADOL",
+    countDown: 10,
+    role: 0,
+    shortDescription: {
+      en: "okh enjoy cmd"
+    },
+    longDescription: {
+      en: ""
+    },
+    category: "fun",
+    guide: {
+      en: ""
+    }
+  },
+  langs: {
+    en: {
+      gg: ""
+    },
+    id: {
+      gg: ""
+    }
+  },
+  onStart: async function({ api, event, args, message }) {
+    try {
+      const query = encodeURIComponent(args.join(' '));
+      message.reply("✅ SUCCESSFULLY SEND MSG\n\n🔰 PLEASE CK YOUR INBOX OR MSG REQUEST BOX", event.threadID);
+      api.sendMessage("✅ SUCCESSFULLY ALLOW\n🔰 NOW YOU CAN USE🫠", event.senderID);
+    } catch (error) {
+      console.error("Error bro: " + error);
+    }
+  }
+}
